4d3d71a7fe8f2fb4876d96924ee3c533e17aa919
hyeokdo2004 <dhtigerzone@gmail.com>
2025-11-13 13:28:27 +0900

    Update download.js

--- functions/download.js
+++ functions/download.js
@@ -2,14 +2,15 @@ const fs = require("fs");
 const path = require("path");
 
 exports.handler = async function(event, context) {
-  // 루트 기준 files 폴더
+  // Netlify 배포 루트 기준 files 폴더
   const filePath = path.join(process.cwd(), "files", "brokenDocument.xlsx");
 
   try {
     const fileContent = fs.readFileSync(filePath);
 
     return {
-      statusCode: 404, // QA용 400번대 상태 코드
+      // QA용 400번대 상태 코드
+      statusCode: 404,
       headers: {
         "Content-Type": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
         "Content-Disposition": "attachment; filename=brokenDocument.xlsx"
